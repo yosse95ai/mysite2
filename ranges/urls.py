fb8aa712df0f0990bd585fbd1b9be7e360d7f832
yosse95ai <kararteyosi@gmai.com>
2021-02-17 10:58:38 +0900

    update

--- ranges/urls.py
+++ ranges/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'ranges'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug:artist_pk>/<slug:song_pk>/detail',views.detail,name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<slug:artist_pk>/<slug:song_pk>/detail/', views.detail, name='detail'),
+    path('search/', views.search, name='search'),
+    path('song-search-result/', views.result, name='result')
 ]
--- ranges/views.py
+++ ranges/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -8,35 +8,76 @@ from django.http import HttpResponse
 from .models import Range, Artist, Song, Note
 
 
-def index(request):
-    all_range_list = Range.objects.all()
-    context = {'all_range_list': all_range_list}
-    return render(request, 'ranges/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'ranges/index.html'
+    context_object_name = 'all_range_list'
+
+    def get_queryset(self):
+        return Range.objects.order_by('-pub_date')[:10]
+
+
+def queryError(error_name):
+    return {error_name: True}
 
 
 def detail(request, artist_pk, song_pk):
-    artist = Artist.objects.get(pk=artist_pk)
-    artist_name = Artist.objects.get(pk=artist_pk)
-    song = Song.objects.get(pk=song_pk)
-    song_name=song.song_name
-    composers = song.composer.all()
-    lyricists = song.lyricist.all()
-    target = Range.objects.get(song=song, artist=artist)
-    lowest_note = Note.objects.get(pk=target.lowest_note.pk)
-    highest_note = Note.objects.get(pk=target.highest_note.pk)
-    performers = target.performer.all()
-    release = target.release_date
-    genre=target.genre
-    context = {
-        'artist': artist_name,
-        'song': song_name,
-        'composers': composers,
-        'lyricists': lyricists,
-        'highest_note': highest_note,
-        'lowest_note': lowest_note,
-        'target': target,
-        'performers': performers,
-        'genre': genre,
-        'release':release,
+    try:
+        artist = get_object_or_404(Artist, pk=artist_pk)
+        song = get_object_or_404(Song, pk=song_pk)
+        target = get_object_or_404(Range, song=song, artist=artist)
+        lowest_note = get_object_or_404(Note, pk=target.lowest_note.pk)
+        highest_note = get_object_or_404(Note, pk=target.highest_note.pk)
+    except Artist.DoesNotExist:
+        context = queryError('artist_error')
+    except Song.DoesNotExist:
+        context = queryError('song_error')
+    except Range.DoesNotExist:
+        context = queryError('range_error')
+    except Note.DoesNotExist:
+        context = queryError('note_error')
+    else:
+        song_name = song.song_name
+        composers = song.composer.all()
+        lyricists = song.lyricist.all()
+        performers = target.performer.all()
+        release = target.release_date
+        genre = target.genre
+        context = {
+            'artist': artist,
+            'song': song_name,
+            'composers': composers,
+            'lyricists': lyricists,
+            'highest_note': highest_note,
+            'lowest_note': lowest_note,
+            'target': target,
+            'performers': performers,
+            'genre': genre,
+            'release': release,
         }
     return render(request, 'ranges/detail.html', context)
+
+
+def search(request):
+    content = {}
+    return render(request, 'ranges/search.html', content)
+
+
+def result(request):
+    input_name = request.POST['song_name']
+    results = list()
+    songs = Song.objects.filter(song_name=input_name)
+    if not songs:
+        context = queryError('song_error')
+    else:
+        for song in songs:
+            try:
+                result = get_list_or_404(Range, song=song.pk)
+            except Range.DoesNotExist:
+                context = queryError('range_error')
+            else:
+                results += result
+                context = {
+                    'results': results,
+                    'input_name': input_name,
+                }
+    return render(request, 'ranges/result.html', context)
